6c772305ee93550c17ef978425d3b6e833b4b268
Farizgeloz <farizgeloz26@gmail.com>
2025-11-24 02:40:26 +0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/frontend-satuadmin/', // <- penting untuk GitHub Pages
   server: {
     port: 3003,      // bisa ganti port permanen
     strictPort: true // error kalau port dipakai, bukan pindah otomatis
